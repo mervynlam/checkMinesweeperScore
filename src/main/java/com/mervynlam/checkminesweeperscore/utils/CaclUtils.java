d2104f7a87a8c78f58da5b4b978662436fb758cd
mervynlam <mervynlam@yeah.net>
2021-10-12 10:35:09 +0800

    优化

--- src/main/java/com/mervynlam/checkminesweeperscore/utils/CaclUtils.java
+++ src/main/java/com/mervynlam/checkminesweeperscore/utils/CaclUtils.java
@@ -7,153 +7,109 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CaclUtils {
     public static void calcScore(int begNum, int intNum, int expNum) {
-        File dir = new File(System.getProperty("user.dir"));
+//        File dir = new File(System.getProperty("user.dir"));
+        File dir = new File("./avf");
         File[] files = dir.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
-                return name.endsWith("avf");
+                return name.endsWith(".avf");
             }
         });
-        List<Double> begTime = new ArrayList<Double>();
-        List<Double> intTime = new ArrayList<Double>();
-        List<Double> expTime = new ArrayList<Double>();
-        List<Double> begBVS = new ArrayList<Double>();
-        List<Double> intBVS = new ArrayList<Double>();
-        List<Double> expBVS = new ArrayList<Double>();
+
+        //录像list
+        List<VideoDisplayBean> begVideos = new ArrayList<>();
+        List<VideoDisplayBean> intVideos = new ArrayList<>();
+        List<VideoDisplayBean> expVideos = new ArrayList<>();
         for (File file : files) {
             VideoDisplayBean bean = Minesweeper.parseVideo(file);
             String level = bean.getLevel();
             String bbbv = bean.getBbbv();
-            String bbbvs = bean.getBbbvs();
-            String time = bean.getTime();
             switch (level) {
                 case "Beg":
                     if (Integer.valueOf(bbbv) < 10) break;
-                    begTime.add(Double.valueOf(time));
-                    begBVS.add(Double.valueOf(bbbvs));
+                    begVideos.add(bean);
                     break;
                 case "Int":
-                    intTime.add(Double.valueOf(time));
-                    intBVS.add(Double.valueOf(bbbvs));
+                    intVideos.add(bean);
                     break;
                 case "Exp":
-                    expTime.add(Double.valueOf(time));
-                    expBVS.add(Double.valueOf(bbbvs));
+                    expVideos.add(bean);
                     break;
             }
         }
-        Comparator<Double> timeComparator = new Comparator<Double>() {
-            @Override
-            public int compare(Double o1, Double o2) {
-                return Double.compare(o1, o2);
-            }
-        };
-        Comparator<Double> bvsComparator = new Comparator<Double>() {
-            @Override
-            public int compare(Double o1, Double o2) {
-                return Double.compare(o2, o1);
-            }
-        };
-        begTime.sort(timeComparator);
-        intTime.sort(timeComparator);
-        expTime.sort(timeComparator);
-        begBVS.sort(bvsComparator);
-        intBVS.sort(bvsComparator);
-        expBVS.sort(bvsComparator);
 
-        double begTimeTotal = 0.0;
-        double intTimeTotal = 0.0;
-        double expTimeTotal = 0.0;
-        double begBVSTotal = 0.0;
-        double intBVSTotal = 0.0;
-        double expBVSTotal = 0.0;
-        double begTimeEdge = 0.0;
-        double intTimeEdge = 0.0;
-        double expTimeEdge = 0.0;
-        double begBVSEdge = 0.0;
-        double intBVSEdge = 0.0;
-        double expBVSEdge = 0.0;
+        //标识set
+        Set<String> userIdSet = new HashSet<>();
+        userIdSet.addAll(getUserId(begVideos));
+        userIdSet.addAll(getUserId(intVideos));
+        userIdSet.addAll(getUserId(expVideos));
 
-        int begCnt = 1;
-        int intCnt = 1;
-        int expCnt = 1;
+        //time bvs list
+        List<Double> begTime = getTimeList(begVideos, begNum);
+        List<Double> begBVS = getBvsList(begVideos, begNum);
+        List<Double> intTime = getTimeList(intVideos, intNum);
+        List<Double> intBVS = getBvsList(intVideos, intNum);
+        List<Double> expTime = getTimeList(expVideos, expNum);
+        List<Double> expBVS = getBvsList(expVideos, expNum);
+
+        //total edge
+        double begTimeTotal = getTotalTime(begTime, begNum, 10);
+        double intTimeTotal = getTotalTime(intTime, intNum, 60);
+        double expTimeTotal = getTotalTime(expTime, expNum, 240);
+        double begBVSTotal = getTotalBvs(begBVS);
+        double intBVSTotal = getTotalBvs(intBVS);
+        double expBVSTotal = getTotalBvs(expBVS);
+        double begTimeEdge = getEdge(begTime, begNum);
+        double intTimeEdge = getEdge(intTime, intNum);
+        double expTimeEdge = getEdge(expTime, expNum);
+        double begBVSEdge = getEdge(begBVS, begNum);
+        double intBVSEdge = getEdge(intBVS, intNum);
+        double expBVSEdge = getEdge(expBVS, expNum);
+
+        //打印信息
+        System.out.println("包含标识：");
+        for (String userId : userIdSet) {
+            System.out.print(userId + "\t");
+        }
+        System.out.println();
+
         System.out.print(String.format("%3s%4s%1s","Beg", "Time", "："));
         for (double begT : begTime) {
             System.out.print(String.format("%6.2f  ",begT));
-            begTimeTotal = add(begTimeTotal, begT);
-            if (begCnt++ >= begNum) {
-                begTimeEdge = begT;
-                break;
-            }
         }
         System.out.println();
         System.out.print(String.format("%3s%4s%1s","Int", "Time", "："));
         for (double intT : intTime) {
             System.out.print(String.format("%6.2f  ",intT));
-            intTimeTotal = add(intTimeTotal, intT);
-            if (intCnt++ >= intNum) {
-                intTimeEdge = intT;
-                break;
-            }
         }
         System.out.println();
         System.out.print(String.format("%3s%4s%1s","Exp", "Time", "："));
         for (double expT : expTime) {
             System.out.print(String.format("%6.2f  ",expT));
-            expTimeTotal = add(expTimeTotal, expT);
-            if (expCnt++ >= expNum) {
-                expTimeEdge = expT;
-                break;
-            }
         }
         System.out.println();
 
-        begCnt = 1;
-        intCnt = 1;
-        expCnt = 1;
         System.out.print(String.format("%3s%4s%1s","Beg", "Bvs", "："));
         for (double begB : begBVS) {
             System.out.print(String.format("%6.2f  ",begB));
-            begBVSTotal = add(begBVSTotal, begB);
-            if (begCnt++ >= begNum) {
-                begBVSEdge = begB;
-                break;
-            }
         }
         System.out.println();
         System.out.print(String.format("%3s%4s%1s","Int", "Bvs", "："));
         for (double intB : intBVS) {
             System.out.print(String.format("%6.2f  ",intB));
-            intBVSTotal = add(intBVSTotal, intB);
-            if (intCnt++ >= intNum) {
-                intBVSEdge = intB;
-                break;
-            }
         }
         System.out.println();
         System.out.print(String.format("%3s%4s%1s","Exp", "Bvs", "："));
         for (double expB : expBVS) {
             System.out.print(String.format("%6.2f  ",expB));
-            expBVSTotal = add(expBVSTotal, expB);
-            if (expCnt++ >= expNum) {
-                expBVSEdge = expB;
-                break;
-            }
         }
         System.out.println();
-        begCnt--;
-        intCnt--;
-        expCnt--;
-        if (begNum - begCnt > 0)
-            begTimeTotal = add(begTimeTotal, 10.0*(begNum - begCnt));
-        if (intNum - intCnt > 0)
-            intTimeTotal = add(intTimeTotal, 60.0*(intNum - intCnt));
-        if (expNum - expCnt > 0)
-            expTimeTotal = add(expTimeTotal, 240.0*(expNum - expCnt));
 
         String tTotal = String.format("%7s：%6.2f + %6.2f + %6.2f = %6.2f", "Time", begTimeTotal, intTimeTotal, expTimeTotal, add(begTimeTotal, add(intTimeTotal, expTimeTotal)));
         String tEdgeTotal = String.format("%7s：%6.2f   %6.2f   %6.2f", "Edge", begTimeEdge, intTimeEdge, expTimeEdge);
@@ -170,4 +126,43 @@ public class CaclUtils {
         BigDecimal b2 = new BigDecimal(Double.toString(v2));
         return b1.add(b2).doubleValue();
     }
+
+    private static int doubleCompare(double v1, double v2) {
+        return Double.compare(v1, v2);
+    }
+
+    private static List<Double> getTimeList(List<VideoDisplayBean> sourceList, int maxNum) {
+        return sourceList.stream().map(bean -> Double.valueOf(bean.getTime()))
+                .limit(maxNum)
+                .sorted((double1, double2) -> doubleCompare(double1, double2))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Double> getBvsList(List<VideoDisplayBean> sourceList, int maxNum) {
+        return sourceList.stream().map(bean -> Double.valueOf(bean.getBbbvs()))
+                .limit(maxNum)
+                .sorted((double1, double2) -> doubleCompare(double2, double1))
+                .collect(Collectors.toList());
+    }
+
+    private static Double getTotalTime(List<Double> timeList, int maxNum, int defaultNum) {
+        double total = timeList.stream().mapToDouble(time -> time).sum();
+        if (timeList.size() < maxNum) {
+            total += (defaultNum * (maxNum - timeList.size()));
+        }
+        return total;
+    }
+
+    private static Double getTotalBvs(List<Double> bvsList) {
+        return bvsList.stream().mapToDouble(bvs -> bvs).sum();
+    }
+
+    private static Double getEdge(List<Double> list, int maxNum) {
+        return list.size()>=maxNum?list.get(maxNum-1):0.0;
+    }
+
+    private static Set<String> getUserId(List<VideoDisplayBean> videoList) {
+        return videoList.stream().map(video -> video.getUserID()).collect(Collectors.toSet());
+    }
+
 }
